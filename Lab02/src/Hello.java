93c44ef557c51edbfc2ec56d6a63267b5b534672
jaimeulloa61 <jaimeulloa61@gmail.com>
2021-03-08 14:24:46 -0800

    Update Hello.java
    Added my name and Sac State ID

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("217644597 - This line was added by Awfa Al-Rakabi.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("217911968 - This line was added by Jaime Ulloa.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
