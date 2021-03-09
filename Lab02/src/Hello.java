d5bd7a45b55a9d8da195db362fd991ec4e9141b3
Nick Patten <nick.patten.r@gmail.com>
2021-03-09 11:34:14 -0800

    Update Hello.java
    Added ID and Name to line 12.

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -9,7 +9,7 @@ public class Hello {
 		System.out.println("217911968 - This line was added by Jaime Ulloa.");
 		System.out.println("218766068 - This line was added by Justin Le.");
 		System.out.println("217529495 - This line was added by Vinh Nguyen.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("217834969 - This line was added by Nicholas Patten.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
