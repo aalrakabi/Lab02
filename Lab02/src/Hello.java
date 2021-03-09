16b509fbc6b401416b6230ae256e68ae8ac9bcd8
Vinh <vinhnguyen3@csus.edu>
2021-03-08 16:25:12 -0800

    Added my Student ID and my name

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -8,7 +8,7 @@ public class Hello {
 		System.out.println("217644597 - This line was added by Awfa Al-Rakabi.");
 		System.out.println("217911968 - This line was added by Jaime Ulloa.");
 		System.out.println("218766068 - This line was added by Justin Le.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("21752495 - This line was added by Vinh Nguyen.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
