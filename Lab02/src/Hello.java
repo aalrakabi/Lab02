de51777cdc71731c90ff48925c7769b68afb7c99
micaelaserranosomavia <micaelaserranosomavia@10.0.0.83>
2021-03-10 09:10:47 -0800

    Added Hello.java file and added my information to line 13.

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -10,6 +10,8 @@ public class Hello {
 		System.out.println("218766068 - This line was added by Justin Le.");
 		System.out.println("217529495 - This line was added by Vinh Nguyen.");
 		System.out.println("217834969 - This line was added by Nicholas Patten.");
+		System.out.println("301696252 - This line was added by Micaela Serrano-Somavia.");
+		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
